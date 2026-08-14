cf0d03ecb28526269bddbca1a591cfae09f3fc1e
himanshu <himanshu.anand.5300@gmail.com>
2026-08-15 04:28:22 +0530

    added Spring JPA, connectivity with postgres

--- src/main/java/com/himanshu/taskmanager/model/Task.java
+++ src/main/java/com/himanshu/taskmanager/model/Task.java
@@ -1,7 +1,15 @@
 package com.himanshu.taskmanager.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Task {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
